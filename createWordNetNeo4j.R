9d72bdb5ac17eaad6c9749b7183c82cdb33afeb7
Brian Loyal <bloyal@gmail.com>
2015-03-22 11:46:46 -0500

    Processing lexnames

--- createWordNetNeo4j.R
+++ createWordNetNeo4j.R
@@ -7,4 +7,22 @@ source('genericGraphFunctions.R');
 graph <- startGraph("http://localhost:7474/db/data/");
 
 #Path to WordNet dict folder
-dict <- "~/Downloads/WordNet-3.0/dict/"
+dictPath <- "~/Downloads/WordNet-3.0/dict";
+
+createLexNodes <- function(graph, dictPath) {
+  getLexNames(dictPath);
+}
+
+getLexNames <- function(dictPath){
+  lexData<-read.table(paste(dictPath,"lexnames", sep="/"), sep="\t", col.names=c("fileNumber","fileName","synCat"),stringsAsFactors=FALSE);
+  lexData$synCat <- updateSynCat(lexData$synCat);
+  return(lexData);
+}
+
+updateSynCat <- function(synCat){
+  synCat <- gsub("1","Noun",synCat);
+  synCat <- gsub("2","Verb",synCat);
+  synCat <- gsub("3","Adjective",synCat);
+  synCat <- gsub("4","Adverb",synCat);
+  return(synCat);
+}
