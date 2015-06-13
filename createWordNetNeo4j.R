c76b88fd8181b6ad3fa2f228826e3233173bcf47
Brian Loyal <bloyal@gmail.com>
2015-06-12 21:12:05 -0500

    added offset finding function

--- createWordNetNeo4j.R
+++ createWordNetNeo4j.R
@@ -1,5 +1,31 @@
 #createWordNetNeo4j.R
 #Functions for creating a WordNet db in Neo4j
+
+#WordNet Release 3.0 This software and database is being provided to you, 
+#the LICENSEE, by Princeton University under the following license. 
+#By obtaining, using and/or copying this software and database, 
+#you agree that you have read, understood, and will comply with 
+#these terms and conditions.: Permission to use, copy, modify and 
+#distribute this software and database and its documentation for 
+#any purpose and without fee or royalty is hereby granted, 
+#provided that you agree to comply with the following copyright notice 
+#and statements, including the disclaimer, and that the same appear 
+#on ALL copies of the software, database and documentation, 
+#including modifications that you make for internal use or for 
+#distribution. WordNet 3.0 Copyright 2006 by Princeton University. 
+#All rights reserved. THIS SOFTWARE AND DATABASE IS PROVIDED "AS IS" 
+#AND PRINCETON UNIVERSITY MAKES NO REPRESENTATIONS OR WARRANTIES, 
+#EXPRESS OR IMPLIED. BY WAY OF EXAMPLE, BUT NOT LIMITATION, 
+#PRINCETON UNIVERSITY MAKES NO REPRESENTATIONS OR WARRANTIES OF 
+#MERCHANT- ABILITY OR FITNESS FOR ANY PARTICULAR PURPOSE OR THAT 
+#THE USE OF THE LICENSED SOFTWARE, DATABASE OR DOCUMENTATION WILL 
+#NOT INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS OR 
+#OTHER RIGHTS. The name of Princeton University or Princeton may 
+#not be used in advertising or publicity pertaining to distribution 
+#of the software and/or database. Title to copyright in this software, 
+#database and any associated documentation shall at all times remain 
+#with Princeton University and LICENSEE agrees to preserve same.
+
 setwd("~/GitHub/RWordNetNeo4j")
 
 library(RNeo4j);
@@ -24,8 +50,13 @@ createLexNodes <- function(graph, dictPath = "~/Downloads/WordNet-3.0/dict") {
 }
 
 readVerbDataFile <- function(path="~/Downloads/WordNet-3.0/dict/data.verb"){
+  #path<-"~/Downloads/WordNet-3.0/dict/data.verb"
   verbData <- readLines(path);
   verbData <- verbData[30:length(verbData)];
+  
+  verbIndex <- readLines("~/Downloads/WordNet-3.0/dict/index.verb");
+  verbIndex <- verbIndex[30:length(verbIndex)];
+  
   lapply(verbData, function(x){ 
     raw<-strsplit(x, " ");
     wordCount<-as.integer(row[[1]][4]);
@@ -81,4 +112,8 @@ createSingleLexNode  <- function(transaction, data){
                synCat = data$synCat,
                description = data$description
   );
+}  
+
+findSynsetData <- function(offset, data){
+  data[grep(paste("^",offset, " .*",sep=""),data)]
 }
