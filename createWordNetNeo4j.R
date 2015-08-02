a17485082fc4f67d9a9a139c22573436dcd4b2df
Brian Loyal <bloyal@gmail.com>
2015-08-02 15:51:01 -0500

    New word node creation strategy
    Leveraged index files to create word nodes and sunset relationships -
    this lets us determine the sense orders for each word

--- createWordNetNeo4j.R
+++ createWordNetNeo4j.R
@@ -31,8 +31,11 @@ createWordNetGraph <- function(dictPath = "~/Downloads/WordNet-3.0/dict", verbos
   
   #Create words
   if(verbose) {print(paste(Sys.time(),"Creating word frame", sep=": "))};
-  wordFrame <- ldply(lapply(wordNetData, getWordFrame));
+  wordFrame<- readPOSWordIndex( "./testData2", verbose=TRUE)
   createWordNodes(graph, wordFrame, verbose=verbose);
+#   if(verbose) {print(paste(Sys.time(),"Creating word frame", sep=": "))};
+#   wordFrame <- ldply(lapply(wordNetData, getWordFrame));
+#   createWordNodes(graph, wordFrame, verbose=verbose);
   
   #Create semantic pointers
   if(verbose) {print(paste(Sys.time(),"Creating pointer frame", sep=": "))};
@@ -252,34 +255,69 @@ createSingleSynsetLexRelationship <- function(transaction, data){
                synsetId = data$synsetId, fileNumber = as.numeric(data$lexFilenum));
 }
 
-#--------------------------------------------------------------------------------------
-# Functions for creating word nodes
-#--------------------------------------------------------------------------------------
+#---------------
+# New functions for creating word nodes
+#---------------
+readPOSWordIndex <- function(dictPath, verbose=TRUE){
+  if(verbose) {print(paste(Sys.time(),"Reading POS word index files", sep=": "))}; 
+  advData<-readAdvWordData(dictPath, verbose);
+  verbData<-readVerbWordData(dictPath, verbose);
+  adjData<-readAdjWordData(dictPath, verbose);
+  nounData<-readNounWordData(dictPath, verbose);
+  ldply(list(adv = advData, verb = verbData, adj = adjData, noun = nounData));
+}
+
+readAdvWordData <- function(path="~/Downloads/WordNet-3.0/dict", verbose=TRUE){
+  if(verbose) {print(paste(Sys.time(),"Reading noun word data", sep=": "))};
+  path<-paste(path,"index.adv",sep="/")
+  readPosWordDataFile(path,"r");
+}
+
+readVerbWordData <- function(path="~/Downloads/WordNet-3.0/dict", verbose=TRUE){
+  if(verbose) {print(paste(Sys.time(),"Reading noun word data", sep=": "))};
+  path<-paste(path,"index.verb",sep="/")
+  readPosWordDataFile(path, "v");
+}
+
+readAdjWordData <- function(path="~/Downloads/WordNet-3.0/dict", verbose=TRUE){
+  if(verbose) {print(paste(Sys.time(),"Reading noun word data", sep=": "))};
+  path<-paste(path,"index.adj",sep="/")
+  readPosWordDataFile(path, "a");
+}
+
+readNounWordData <- function(path="~/Downloads/WordNet-3.0/dict", verbose=TRUE){
+  if(verbose) {print(paste(Sys.time(),"Reading noun word data", sep=": "))};
+  path<-paste(path,"index.noun",sep="/")
+  readPosWordDataFile(path, "n");
+}
+
+readPosWordDataFile <- function(path="~/Downloads/WordNet-3.0/dict/data.verb", posCode="n"){
+  posData <- readLines(path);
+  posData <- posData[30:length(posData)];
+  processWordData(posData, posCode);
+}
+
+processWordData <- function(wordData,posCode){
+  #print(wordData);
+  words<-ldply(str_match_all(wordData,"^(\\S+) ([arnv] .+ \\d) (\\d{8}.*)"))[,c(2,4)];
+  names(words)=c("name", "synsetOffset");
+  #print(words);
+  ldply(apply(words, 1, function(line){
+    #print(line["synsetOffset"]);
+    synsetOffsets<-str_match_all(line["synsetOffset"], "(\\S{8})")[[1]][,1];
+    df<-data.frame(name=str_replace_all(str_to_lower(line["name"]),"_"," "), synsetId=calcSynsetId(synsetOffsets,posCode), stringsAsFactors = FALSE, row.names=NULL);
+    cbind(df,wordNum=as.numeric(rownames(df)));
+  }));
+}
 
 createWordNodes <- function(graph, wordFrame, verbose=TRUE){
-  addIndex(graph, "Word", "name");
+  #addIndex(graph, "Word", "name");
   #print(typeof(wordFrame));
+  print(wordFrame);
   if(verbose) {print(paste(Sys.time(),"Creating word nodes", sep=": "))};
   bulkGraphUpdate(graph, wordFrame, createSingleWordNode);
   if(verbose) {print(paste(Sys.time(),"Creating synset-word relationships", sep=": "))};
   bulkGraphUpdate(graph, wordFrame, createSingleSynsetWordRelationship);
-}
-
-#Create word frame
-getWordFrame <- function(synsetData){
-  z<-apply(synsetData, 1, transformSynsetDataToWordMap)
-  ldply(z)
-}
-
-#process single line of synset data (inside apply) to create a narrow data frame with the offset and words
-transformSynsetDataToWordMap <- function(synsetLine){
-  offset<-synsetLine["synsetOffset"];
-  pos<-synsetLine["pos"];
-  words<-str_replace_all(str_to_lower(str_match_all(synsetLine["words"], "(\\S+) [0-9a-f]")[[1]][,2]),"_"," ");
-  #words<-str_to_lower(str_match_all(synsetLine["words"], "(\\S+) [0-9a-f]")[[1]][,2]);
-  #df<-data.frame(synsetOffset=offset, pos=pos, name=words, stringsAsFactors=FALSE, row.names=NULL);
-  df<-data.frame(synsetId=calcSynsetId(offset, pos), name=words, stringsAsFactors=FALSE, row.names=NULL);
-  cbind(df,wordNum=as.numeric(rownames(df)));
 }
 
 createSingleWordNode <- function(transaction, data){
@@ -289,12 +327,57 @@ createSingleWordNode <- function(transaction, data){
 
 createSingleSynsetWordRelationship <- function(transaction, data){
   #print(data)
-#   query <- "MATCH (a:Synset {synsetOffset:{synsetOffset}, pos:{pos}}), (b:Word {name:{name}})
-#   CREATE (a)-[:has_word {wordNum:{wordNum}}]->(b)";  
-  query <- "MATCH (a:Synset {synsetId:{synsetId}}), (b:Word {name:{name}})
-  CREATE (a)-[:has_word {wordNum:{wordNum}}]->(b)"; 
+  #   query <- "MATCH (a:Synset {synsetOffset:{synsetOffset}, pos:{pos}}), (b:Word {name:{name}})
+  #   CREATE (a)-[:has_word {wordNum:{wordNum}}]->(b)";  
+  query <- "MATCH (a:Word {name:{name}}), (b:Synset {synsetId:{synsetId}})
+  CREATE (a)-[:has_synset {wordNum:{wordNum}}]->(b)"; 
   appendCypher(transaction, query, synsetId = data$synsetId, name = data$name, wordNum = data$wordNum);
 }
+
+
+#--------------------------------------------------------------------------------------
+# Functions for creating word nodes
+#--------------------------------------------------------------------------------------
+# 
+# createWordNodes <- function(graph, wordFrame, verbose=TRUE){
+#   addIndex(graph, "Word", "name");
+#   #print(typeof(wordFrame));
+#   if(verbose) {print(paste(Sys.time(),"Creating word nodes", sep=": "))};
+#   bulkGraphUpdate(graph, wordFrame, createSingleWordNode);
+#   if(verbose) {print(paste(Sys.time(),"Creating synset-word relationships", sep=": "))};
+#   bulkGraphUpdate(graph, wordFrame, createSingleSynsetWordRelationship);
+# }
+# 
+# #Create word frame
+# getWordFrame <- function(synsetData){
+#   z<-apply(synsetData, 1, transformSynsetDataToWordMap)
+#   ldply(z)
+# }
+# 
+# #process single line of synset data (inside apply) to create a narrow data frame with the offset and words
+# transformSynsetDataToWordMap <- function(synsetLine){
+#   offset<-synsetLine["synsetOffset"];
+#   pos<-synsetLine["pos"];
+#   words<-str_replace_all(str_to_lower(str_match_all(synsetLine["words"], "(\\S+) [0-9a-f]")[[1]][,2]),"_"," ");
+#   #words<-str_to_lower(str_match_all(synsetLine["words"], "(\\S+) [0-9a-f]")[[1]][,2]);
+#   #df<-data.frame(synsetOffset=offset, pos=pos, name=words, stringsAsFactors=FALSE, row.names=NULL);
+#   df<-data.frame(synsetId=calcSynsetId(offset, pos), name=words, stringsAsFactors=FALSE, row.names=NULL);
+#   cbind(df,wordNum=as.numeric(rownames(df)));
+# }
+# 
+# createSingleWordNode <- function(transaction, data){
+#   query <- "MERGE (:Word {name:{name}})";  
+#   appendCypher(transaction, query, name = data$name);
+# }
+# 
+# createSingleSynsetWordRelationship <- function(transaction, data){
+#   #print(data)
+# #   query <- "MATCH (a:Synset {synsetOffset:{synsetOffset}, pos:{pos}}), (b:Word {name:{name}})
+# #   CREATE (a)-[:has_word {wordNum:{wordNum}}]->(b)";  
+#   query <- "MATCH (a:Synset {synsetId:{synsetId}}), (b:Word {name:{name}})
+#   CREATE (a)-[:has_word {wordNum:{wordNum}}]->(b)"; 
+#   appendCypher(transaction, query, synsetId = data$synsetId, name = data$name, wordNum = data$wordNum);
+# }
 
 #--------------------------------------------------------------------------------------
 # Functions for creating semantic pointers
